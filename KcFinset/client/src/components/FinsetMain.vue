6ba0b5a5c99480fcd2c39bbb8c17e40f0213ced8
JSP <JSP@JSP-PC>
2018-09-01 18:56:09 +0900

    add

---
+++ KcFinset/client/src/components/FinsetMain.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        <!-- <MainHeader></rMainHeader> -->
+        <router-view></router-view>
+        <!-- <MainBottom></MainBottom> -->
+    </main>
+</template>
+
+<script>
+
+
+// import  MainBottom  from   './../common/MainBottom.vue'
+export default {
+    components: {
+        // MainBottom : MainBottom,
+    }
+}
+</script>
+
+<style scoped>
+</style>

679a87de633cd6b4ea65badbe0fbeae9c7cdda24
JSP <JSP@JSP-PC>
2018-09-04 17:14:21 +0900

    sample

---
+++ KcFinset/client/src/components/common/FinsetBottom.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="menu-fixed-bottom">
+    <ul class="menu-list">
+      <li id="footer_credit" class=""><a class="menu-credit">신용관리</a></li>
+      <li id="footer_debt"><a class="menu-debt">부채관리</a></li>
+      <li id="footer_loan" class=""><a class="menu-loan">상품조회</a></li>
+      <li id="footer_mypage"><a class="menu-mypage">마이페이지</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FinsetBottom',
+  data () {
+    return {
+    }
+  },
+  component: {
+  },
+  computed () {
+  },
+  beforeCreate() {
+  },
+  created () {
+  },
+  beforeMount() {
+  },
+  mounted () {
+  },
+  beforeUpdate() {
+  },
+  updated() {
+  },
+  beforeDestroy () {
+  },
+  destroyed() {
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ KcFinset/client/src/components/common/FinsetHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id='wrapper'>
+    <!-- Header -->
+    <header id='header'>
+      <div class='input-group'>
+        <div class='input-group-btn blind'>
+          <button type='button' class='ui-nav nav-back'>뒤로가기</button>
+        </div>
+        <h1>신용관리</h1>
+        <div class='g-menu'>
+          <button type='button' class='ico ico-notilist'>알림내역</button>
+          <button type='button' class='btn btn-gmenu'>정보공유</button>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FinsetHeader',
+  data () {
+    return {
+    }
+  },
+  component: {
+  },
+  computed () {
+  },
+  beforeCreate() {
+  },
+  created () {
+  },
+  beforeMount() {
+  },
+  mounted () {
+  },
+  beforeUpdate() {
+  },
+  updated() {
+  },
+  beforeDestroy () {
+  },
+  destroyed() {
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

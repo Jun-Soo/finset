9ffe65fa5f22e6407620dab96d9b449a4b89b395
khk9742 <hkkim@vitcom.co.kr>
2018-09-05 14:46:13 +0900

    금융사 추가 로직 추가

--- KcFinset/src/main/java/com/koscom/fincorp/dao/FincorpMapper.java
+++ KcFinset/src/main/java/com/koscom/fincorp/dao/FincorpMapper.java
@@ -3,6 +3,7 @@ package com.koscom.fincorp.dao;
 import java.util.HashMap;
 
 import com.koscom.fincorp.model.FincorpVO;
+import com.koscom.fincorp.model.FincorpfcNminfoForm;
 
 public interface FincorpMapper {
 	
@@ -27,4 +28,17 @@ public interface FincorpMapper {
 	 */
 	@SuppressWarnings("rawtypes")
 	HashMap getImgBi(FincorpVO vo);
+	
+	/**
+	 * 금융사 추가 (KcbManagerImpl)
+	 * @param FincorpVO
+	 */
+	int createFincorp(FincorpVO fincorpVO);
+	
+	/**
+	 * 금융사명 KCB 사전연계 정보 삽입 (KcbManagerImpl)
+	 * @param fincorpfcNminfoForm
+	 * @return String
+	 */
+	int createFincorpfcNminfo(FincorpfcNminfoForm fincorpfcNminfoForm);
 }

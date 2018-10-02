687e212ec793600cfdc916934a07cb839a88fab3
minkyng <minkyoung9203@gmail.com>
2018-10-02 16:31:25 +0900

    vue-chart Example

---
+++ KcFinset/client/src/components/template/ChartMultipleBar.vue
@@ -0,0 +1,12 @@
+<template>
+        <div style="margin-top:100px;">
+        <canvas id="mycanvas" count="3"></canvas>
+        <chartjs-bar target="mycanvas"></chartjs-bar>
+        <chartjs-bar target="mycanvas" :backgroundcolor="'#383838'"></chartjs-bar>
+        <chartjs-bar target="mycanvas"></chartjs-bar>
+        </div>
+</template>
+
+<script type="text/javascript">
+   
+</script>
---
+++ KcFinset/client/src/components/template/Main.vue
@@ -0,0 +1,62 @@
+<template>
+  <div style="margin-top:100px;">
+    <h1>
+      <b>Chart</b><br/>
+      <a href="http://vue-charts.hchspersonal.tk/" style="cursor:pointer;" target="_blank">vueChartHome</a><br/>
+      <a href="/template/chartSingleLine" style="cursor:pointer;" target="_blank">singleLineExample</a><br/>
+      <a href="/template/chartSingleLine2" style="cursor:pointer;" target="_blank">singleLine2Example</a><br/>
+      <a href="/template/chartMultipleBar" style="cursor:pointer;" target="_blank">MultipleBarExample</a><br/>
+    </h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'templateMain',
+  data() {
+    return {
+      errMsg: ''
+    }
+  },
+  component: {
+  },
+  computed: {
+  },
+  beforeCreate() {
+  },
+  created() {
+  },
+  beforeMount() {
+  },
+  mounted() {
+  },
+  beforeUpdate() {
+  },
+  updated() {
+  },
+  beforeDestroy() {
+  },
+  destroyed() {
+  },
+  methods: {
+    // goHcHsChartsHome: function(){
+    //   location.href = "/chartExam/sumMain"
+    // },
+    // goSingleLine: function(){
+
+    // },
+    // goSingleLine2: function(){
+
+    // },
+    // goMultiBar: function(){
+
+    // }
+  }
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style lang="scss">
+
+</style>

192319d4c2c8b75c8fa557ba96d67b55b5f2c69e
minkyng <minkyoung9203@gmail.com>
2018-10-23 17:52:35 +0900

    date형식 dot 추가

--- KcFinset/client/src/assets/js/common.js
+++ KcFinset/client/src/assets/js/common.js
@@ -230,6 +230,35 @@ export default {
       return undefined
     }
   },
+  formatDateDot: function (date, pattern) {
+    var yyyy = ''
+    var mm = ''
+    var dd = ''
+    if (typeof date === 'string') {
+      yyyy = date.substring(0, 4)
+      mm = date.substring(4, 6)
+      dd = date.substring(6, 8)
+    } else if (typeof date === 'object') {
+      yyyy = date.getFullYear()
+      mm = date.getMonth() + 1
+      if (mm < 10) {
+        mm = '0' + mm
+      }
+      dd = date.getDate()
+      if (dd < 10) {
+        dd = '0' + dd
+      }
+    }
+    if (((pattern || '') === '') || pattern === 'yyyymmdd') {
+      return yyyy + '.' + mm + '.' + dd
+    } else if (pattern === 'yyyymm') {
+      return yyyy + '.' + mm
+    } else if (pattern === 'mmdd') {
+      return mm + '.' + dd
+    } else {
+      return undefined
+    }
+  },
   getCodeName: function (group, code) {
     var data = {
       'group': group,

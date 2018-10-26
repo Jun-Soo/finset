e904bc20b5e0aa624b1a4e5c5ed1be8feae36df2
우상윤 <woosy@thecrex.com>
2018-10-26 15:45:14 +0900

    script 수정

--- KcFinset/client/src/assets/js/jquery.js
+++ KcFinset/client/src/assets/js/jquery.js
@@ -4,6 +4,8 @@ $(function () {
   $(document).on('click', 'aside button', function () {
     $('aside').toggleClass('on')
   })
+  /*   GNB 로드   */
+  $('header').load('../include/gnb.html')
 
   $(document).on('click', 'header .open-menu', function () {
     $('.gnb-wrap').addClass('on')
@@ -24,11 +26,76 @@ $(function () {
     $(this).toggleClass('on')
   })
 
-  // $(document).on('click', '.btn-search', function () {
-  //   $('aside.search-wrap').addClass('on')
-  // })
+  $(document).on('click', '.btn-search', function () {
+    $('aside.search-wrap').addClass('on')
+  })
 
   $(document).on('click', '.search-wrap .top button', function () {
     $('aside.search-wrap').removeClass('on')
+  })
+
+  $(document).on('click', '.btn-menu-pop', function () {
+    $(this).closest('.btn-menu-wrap').toggleClass('on')
+  })
+
+  $(document).on('click', '.accodion .top a', function (e) {
+    $(this).closest('li').toggleClass('on')
+    e.preventDefault()
+  })
+
+  $(document).on('click', '.btn-onoff', function (e) {
+    $(this).toggleClass('on')
+  })
+
+  function modal () {
+    var openBtn = '[data-modal]'
+    var closeBtn = '.modal-close'
+
+    function getTarget (t) {
+      return $(t).attr('data-modal')
+    }
+
+    function open (t) {
+      var showTarget = $('[data-modal-con= + t + ]')
+
+      window.setTimeout(function () {
+        var popHeight = showTarget.height() / -2
+        var popWidth = showTarget.innerWidth() / -2
+        showTarget.attr('tabindex', '0')
+        showTarget.show().css({
+          'margin-top': popHeight,
+          'margin-left': popWidth
+        })
+        showTarget.focus()
+        showTarget.find('.modal-close').data('activeTarget', t)
+      }, 500)
+    }
+
+    function close (t) {
+      var activeTarget = $('[data-modal-con= + t + ]')
+      activeTarget.hide()
+      $('[data-modal=' + t + ']').focus()
+    }
+
+    $(document).on('click', openBtn, function (e) {
+      e.preventDefault()
+      open(getTarget($(this)))
+      $('#modal-wrap').show()
+    }).on('click', closeBtn, function (e) {
+      e.preventDefault()
+      close($(this).data('activeTarget'))
+      $('#modal-wrap').hide()
+    })
+  }
+
+  modal()
+
+  var owl = $('.owl-carousel')
+
+  owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 21,
+    padding: 21
   })
 })

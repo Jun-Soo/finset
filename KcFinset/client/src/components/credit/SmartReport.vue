31680eeccff3c89aadf6bba3a87c464b6ff25885
박준수 <ysmspjs@naver.com>
2018-11-22 18:57:56 +0900

    css 충돌 삭제

--- KcFinset/client/src/components/credit/SmartReport.vue
+++ KcFinset/client/src/components/credit/SmartReport.vue
@@ -1,18 +1,18 @@
 <template>
   <section>
-		<div class="progress-wrap scraping show">
-			<div class="loader">
-				<svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
-					<path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
-						<animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"/>
-					</path>
-				</svg>
-			</div>
-		</div>
-		<!-- Content -->
-		<div id="content" class="ifm-content" style="overflow-x:hidden;overflow-y:scroll;width:100%;height:1400px;">
-			<iframe id="ifrContents" name="ifrContents" scrolling="no" frameborder="0"></iframe>	
-		</div>
+    <div class="progress-wrap scraping show">
+      <div class="loader">
+        <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+          <path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
+            <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite" />
+          </path>
+        </svg>
+      </div>
+    </div>
+    <!-- Content -->
+    <div id="content" class="ifm-content" style="overflow-x:hidden;overflow-y:scroll;width:100%;height:1400px;">
+      <iframe id="ifrContents" name="ifrContents" scrolling="no" frameborder="0"></iframe>
+    </div>
   </section>
 </template>
 
@@ -110,10 +110,10 @@ export default {
 /*
   Set the color of the icon
 */
-svg path,
-svg rect {
-  fill: #2b43ba;
-}
+// svg path,
+// svg rect {
+//   fill: #2b43ba;
+// }
 .progress-txt {
   margin-top: 50px;
 }

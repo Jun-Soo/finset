8aa5d1c824afe5dc7f3ace61ba49e94ab8cac3b2
khk9742 <hkkim@vitcom.co.kr>
2018-12-13 14:19:28 +0900

    모달창이 viewpoint에 딱 맞춰서 나오지 않고 전체 div에 잡히도록 설정

--- KcFinset/client/src/components/plugins/modal/Popup.vue
+++ KcFinset/client/src/components/plugins/modal/Popup.vue
@@ -228,13 +228,13 @@ $mobileSize: "600px";
 
   &__wrapper {
     width: 100%;
-    height: 100%;
+    // height: 100%;
   }
 
   &__content {
     margin: 0px auto;
-    margin-bottom: 5%;
-    margin-top: 5%;
+    // margin-bottom: 5%;
+    // margin-top: 5%;
     width: 50%;
     height: 100%;
     background-color: #fff;

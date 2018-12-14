1d7a46deced86588241571839e7cf8ee7e2175d3
minkyng <minkyoung9203@gmail.com>
2018-12-14 16:37:45 +0900

    자산관리_은행메인, 증권메인 정렬하기set버튼 추가

--- KcFinset/client/src/components/assets/StockMain.vue
+++ KcFinset/client/src/components/assets/StockMain.vue
@@ -16,51 +16,80 @@
         </div>
       </div>
 
-      <div v-if="accountMyList.length == 0 && accountShareList.length == 0" class="nodata">계좌 내역이 없습니다</div>
+      <div class="align-item mb20">
+        <em class="mr10">정렬하기</em><button @click="setOrder();" class="btn-onoff" :class="{on: isSetOrder}" :disabled="accountList.length==0"></button>
+      </div>
 
-      <draggable v-model="accountMyList" @start="drag=true" :options="draggableOptions" @update="changeSort()">
-        <div v-for="accountMyInfo in accountMyList" :key="accountMyInfo.index" class="item sortClass">
-          <a @click="viewDetail(accountMyInfo.no_person, accountMyInfo.no_account, accountMyInfo.nm_detail_class)" class="block handle">
+      <template v-if="isSetOrder">
+        <div v-if="accountMyList.length == 0 && accountShareList.length == 0" class="nodata">계좌 내역이 없습니다</div>
+
+        <draggable v-model="accountMyList" @start="drag=true" :options="draggableOptions" @update="changeSort()">
+          <div v-for="accountMyInfo in accountMyList" :key="accountMyInfo.index" class="item sortClass">
+            <a @click="viewDetail(accountMyInfo.no_person, accountMyInfo.no_account, accountMyInfo.nm_detail_class)" class="block handle">
+              <div class="top">
+                <p class="symbol"><img :src="accountMyInfo.fcImg" alt="" />{{accountMyInfo.nm_fc}}</p>
+                <p v-if="personShareList.length!=0" class="text"><span class="circle" :class="colorList[accountMyInfo.rk]">{{accountMyInfo.nm_person}}</span></p>
+              </div>
+              <div class="number-wrap bi">
+                <div class="left">
+                  <p class="number">{{formatNumber(accountMyInfo.amt_stock)}}<em>원</em></p>
+                </div>
+                <div class="right">
+                  <p class="value blue">{{accountMyInfo.proloss}} ({{accountMyInfo.rate_return}}%)</p>
+                </div>
+              </div>
+              <div class="goods-cate nor">
+                <em>{{accountMyInfo.nm_detail_class}}</em>
+                <em>{{accountMyInfo.no_account}}</em>
+              </div>
+            </a>
+          </div>
+        </draggable>
+
+        <div v-for="accountShareInfo in accountShareList" :key="accountShareInfo.index" class="item">
+          <a @click="viewDetail(accountShareInfo.no_person, accountShareInfo.no_account, accountShareInfo.nm_detail_class)" class="block">
             <div class="top">
-              <p class="symbol"><img :src="accountMyInfo.fcImg" alt="" />{{accountMyInfo.nm_fc}}</p>
-              <p v-if="personShareList.length!=0" class="text"><span class="circle" :class="colorList[accountMyInfo.rk]">{{accountMyInfo.nm_person}}</span></p>
+              <p class="symbol"><img :src="accountShareInfo.fcImg" alt="" />{{accountShareInfo.nm_fc}}</p>
+              <p class="text"><span class="circle" :class="colorList[accountShareInfo.rk]">{{accountShareInfo.nm_person}}</span></p>
             </div>
             <div class="number-wrap bi">
               <div class="left">
-                <p class="number">{{formatNumber(accountMyInfo.amt_stock)}}<em>원</em></p>
+                <p class="number">{{accountShareInfo.amt_stock}}<em>원</em></p>
               </div>
               <div class="right">
-                <p class="value blue">{{accountMyInfo.proloss}} ({{accountMyInfo.rate_return}}%)</p>
+                <p class="value blue">{{accountShareInfo.proloss}} ({{accountShareInfo.rate_return}}%)</p>
               </div>
             </div>
             <div class="goods-cate nor">
-              <em>{{accountMyInfo.nm_detail_class}}</em>
-              <em>{{accountMyInfo.no_account}}</em>
+              <em>{{accountShareInfo.nm_detail_class}}</em>
+              <em>{{accountShareInfo.no_account}}</em>
             </div>
           </a>
         </div>
-      </draggable>
-
-      <div v-for="accountShareInfo in accountShareList" :key="accountShareInfo.index" class="item">
-        <a @click="viewDetail(accountShareInfo.no_person, accountShareInfo.no_account, accountShareInfo.nm_detail_class)" class="block">
-          <div class="top">
-            <p class="symbol"><img :src="accountShareInfo.fcImg" alt="" />{{accountShareInfo.nm_fc}}</p>
-            <p class="text"><span class="circle" :class="colorList[accountShareInfo.rk]">{{accountShareInfo.nm_person}}</span></p>
-          </div>
-          <div class="number-wrap bi">
-            <div class="left">
-              <p class="number">{{accountMyInfo.amt_stock}}<em>원</em></p>
+      </template>
+      <template v-else>
+        <div v-if="accountList.length == 0" class="nodata">계좌 내역이 없습니다</div>
+        <div v-for="accountInfo in accountList" :key="accountInfo.index" class="item">
+          <a @click="viewDetail(accountInfo.no_person, accountInfo.no_account, accountInfo.nm_detail_class)" class="block">
+            <div class="top">
+              <p class="symbol"><img :src="accountInfo.fcImg" alt="" />{{accountInfo.nm_fc}}</p>
+              <p v-if="personShareList.length!=0" class="text"><span class="circle" :class="colorList[accountInfo.rk]">{{accountInfo.nm_person}}</span></p>
             </div>
-            <div class="right">
-              <p class="value blue">{{accountMyInfo.proloss}} ({{accountMyInfo.rate_return}}%)</p>
+            <div class="number-wrap bi">
+              <div class="left">
+                <p class="number">{{accountInfo.amt_stock}}<em>원</em></p>
+              </div>
+              <div class="right">
+                <p class="value blue">{{accountInfo.proloss}} ({{accountInfo.rate_return}}%)</p>
+              </div>
             </div>
-          </div>
-          <div class="goods-cate nor">
-            <em>{{accountMyInfo.nm_detail_class}}</em>
-            <em>{{accountMyInfo.no_account}}</em>
-          </div>
-        </a>
-      </div>
+            <div class="goods-cate nor">
+              <em>{{accountInfo.nm_detail_class}}</em>
+              <em>{{accountInfo.no_account}}</em>
+            </div>
+          </a>
+        </div>
+      </template>
     </div>
 
     <div class="btn-wrap float">
@@ -87,6 +116,7 @@ export default {
         touchStartThreshold: 200
       },
       sumAmt: "", //총금액
+      isSetOrder: false, //정렬셋팅OnOff
       colorList: ["red", "orange", "green", "blue", "purple"],
       personShareList: [], //공유자list
       person_share_list: [], //공유자list(조회용)
@@ -169,12 +199,13 @@ export default {
           }
 
           //pagination
+          if (_this.page == 1) {
+            _this.accountMyList = [];
+            _this.accountShareList = [];
+            _this.accountList = [];
+          }
+
           if (list.length === 0) {
-            if (_this.page == 1) {
-              _this.accountMyList = [];
-              _this.accountShareList = [];
-              _this.accountList = [];
-            }
             callback();
             _this.seen = true;
             return;
@@ -189,6 +220,8 @@ export default {
                 _this.accountShareList.push(list[key]);
               }
             }
+
+            _this.accountList = list;
           } else {
             for (var key in list) {
               if ("0" == list[key].rk) {
@@ -196,6 +229,8 @@ export default {
               } else {
                 _this.accountShareList.push(list[key]);
               }
+
+              _this.accountList.push(list[key]);
             }
           }
 
@@ -211,6 +246,12 @@ export default {
     },
     formatNumber: function(data) {
       return Common.formatNumber(data);
+    },
+    //정렬하기 설정
+    setOrder: function() {
+      var _this = this;
+      _this.isSetOrder = !_this.isSetOrder;
+      _this.searchAccountList();
     },
     //정렬순서 변경
     changeSort: function() {

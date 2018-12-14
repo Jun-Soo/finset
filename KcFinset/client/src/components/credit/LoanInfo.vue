e429951e857d0235a9403155a558c3a5fb3418bd
minkyng <minkyoung9203@gmail.com>
2018-12-14 13:43:44 +0900

    신용관리_대출현황 상세페이지 링크이동구현

--- KcFinset/client/src/components/credit/LoanInfo.vue
+++ KcFinset/client/src/components/credit/LoanInfo.vue
@@ -1,60 +1,64 @@
 <template>
   <section>
     <div class="container">
-        <div v-if="debtList.length == 0" class="nodata">등록 내역이 없습니다</div>
-        <div v-else class="bar-top">
-              <p class="key">대출잔액</p>
-              <div class="wrap">
-                  <div class="left">{{formatNumber(ds_sum_amt_remain)}}<em>원</em></div>
-                  <div class="right">
-                      <p class="key">대출금액</p>
-                      <p class="value">{{formatNumber(Math.round(ds_sum_amt_contract/10000))}} <em>만원</em></p>
-                  </div>
-              </div>
-              <div class="bar"><p :style="{width: ds_tot_repay_pc+'%'}"></p></div>
-              <div class="bar-text-wrap">
-                  <p class="bar-text">대출금 상환비율<em>{{ds_tot_repay_pc}}%</em></p>
-                  <p class="bar-card">보유갯수<em>{{ds_debt_cnt}}</em></p>
-              </div>
+      <div v-if="debtList.length == 0" class="nodata">등록 내역이 없습니다</div>
+      <div v-else class="bar-top">
+        <p class="key">대출잔액</p>
+        <div class="wrap">
+          <div class="left">{{formatNumber(ds_sum_amt_remain)}}<em>원</em></div>
+          <div class="right">
+            <p class="key">대출금액</p>
+            <p class="value">{{formatNumber(Math.round(ds_sum_amt_contract/10000))}} <em>만원</em></p>
           </div>
+        </div>
+        <div class="bar">
+          <p :style="{width: ds_tot_repay_pc+'%'}"></p>
+        </div>
+        <div class="bar-text-wrap">
+          <p class="bar-text">대출금 상환비율<em>{{ds_tot_repay_pc}}%</em></p>
+          <p class="bar-card">보유갯수<em>{{ds_debt_cnt}}</em></p>
+        </div>
       </div>
-      
-      <div v-if="debtList.length != 0" class="box-list list01">
-          <div v-for="debtInfo in debtList" :key="debtInfo.index" class="item" @click="viewDetailDebt(debtInfo.no_manage_info)">
-              <div class="top">
-                  <p class="symbol"><img :src="debtInfo.fcImg" alt=""/>{{debtInfo.nm_fc}}</p>
-                  <p class="text blue">{{debtInfo.cd_debt == '01'? '신용' : '담보'}}</p>
-              </div>
-              <div class="number-wrap">
-                  <div class="left">
-                      <p class="key">잔액</p>
-                      <p class="number">{{formatNumber(debtInfo.amt_remain)}}<em>원</em></p>
-                  </div>
-              </div>
-              <div class="bar"><p :style="{width: debtInfo.repayPc+'%'}"></p></div>
-              <div class="text-wrap">
-                  <div class="left">
-                      <p class="key">대출금액</p>
-                      <p class="value">{{formatNumber(Math.round(debtInfo.amt_contract/10000))}} <em>만원</em></p>
-                  </div>
-                  <div class="right">
-                      <p class="key">개설일자</p>
-                      <p class="value">{{formatDateDot(debtInfo.ymd_loan)}}</p>
-                  </div>
-              </div>
-          </div>
-          
-          <div class="btn-wrap">
-              <a @click="$router.push('/debt/main')" class="solid blue">자세히보기</a>
-          </div>
+    </div>
 
-          <!-- noti -->
-          <div class="info-massage">대출개설정보는 현재 해지되지 않은 대출정보입니다.</div>
-					<div class="info-massage">일부 기관의 대출정보는 “개인신용정보 제공 및 활용동의서” 미제출 경우거나 해당 금융기관의 기준 및 정책에 의해 대출정보가 제공되지 않을 수 있습니다.</div>
-					<div class="info-massage">금융사로부터 정보가 등록되지 않은 경우 대출 이용 내역 등 정보가 공란으로 표기될 수 있습니다.</div>
-					<div class="info-massage">“개인신용정보 제공 및 활용동의서” 미제출한 경우거나 해당 금융 기관의 기준 및 정책에 의해 일부 카드 정보가 제공되지 않을 수 있습니다.</div>
-					<div class="info-massage">금융사로부터 정보가 등록되지 않은 경우 대출 이용 내역 등 정보가 공란으로 표기될 수 있습니다.</div>
+    <div v-if="debtList.length != 0" class="box-list list01">
+      <div v-for="debtInfo in debtList" :key="debtInfo.index" class="item" @click="viewDetailDebt(debtInfo.no_manage_info)">
+        <div class="top">
+          <p class="symbol"><img :src="debtInfo.fcImg" alt="" />{{debtInfo.nm_fc}}</p>
+          <p class="text blue">{{debtInfo.cd_debt == '01'? '신용' : '담보'}}</p>
+        </div>
+        <div class="number-wrap">
+          <div class="left">
+            <p class="key">잔액</p>
+            <p class="number">{{formatNumber(debtInfo.amt_remain)}}<em>원</em></p>
+          </div>
+        </div>
+        <div class="bar">
+          <p :style="{width: debtInfo.repayPc+'%'}"></p>
+        </div>
+        <div class="text-wrap">
+          <div class="left">
+            <p class="key">대출금액</p>
+            <p class="value">{{formatNumber(Math.round(debtInfo.amt_contract/10000))}} <em>만원</em></p>
+          </div>
+          <div class="right">
+            <p class="key">개설일자</p>
+            <p class="value">{{formatDateDot(debtInfo.ymd_loan)}}</p>
+          </div>
+        </div>
       </div>
+
+      <div class="btn-wrap">
+        <a @click="$router.push('/debt/main')" class="solid blue">자세히보기</a>
+      </div>
+
+      <!-- noti -->
+      <div class="info-massage">대출개설정보는 현재 해지되지 않은 대출정보입니다.</div>
+      <div class="info-massage">일부 기관의 대출정보는 “개인신용정보 제공 및 활용동의서” 미제출 경우거나 해당 금융기관의 기준 및 정책에 의해 대출정보가 제공되지 않을 수 있습니다.</div>
+      <div class="info-massage">금융사로부터 정보가 등록되지 않은 경우 대출 이용 내역 등 정보가 공란으로 표기될 수 있습니다.</div>
+      <div class="info-massage">“개인신용정보 제공 및 활용동의서” 미제출한 경우거나 해당 금융 기관의 기준 및 정책에 의해 일부 카드 정보가 제공되지 않을 수 있습니다.</div>
+      <div class="info-massage">금융사로부터 정보가 등록되지 않은 경우 대출 이용 내역 등 정보가 공란으로 표기될 수 있습니다.</div>
+    </div>
   </section>
 </template>
 
@@ -108,7 +112,8 @@ export default {
             if (debtSumInfo.sum_amt_contract > 0) {
               _this.ds_tot_repay_pc = Math.round(
                 ((debtSumInfo.sum_amt_contract - debtSumInfo.sum_amt_remain) /
-                  debtSumInfo.sum_amt_contract) * 100
+                  debtSumInfo.sum_amt_contract) *
+                  100
               );
             }
           }
@@ -142,7 +147,11 @@ export default {
     viewDetailDebt: function(no_manage_info) {
       this.$router.push({
         name: "debtDetail",
-        params: { no_manage_info: no_manage_info }
+        query: {
+          no_person: this.$store.state.user.noPerson,
+          no_manage_info: no_manage_info,
+          isMine: true
+        }
       });
     },
     formatNumber: function(data) {

2dedfbe2c7b2220a5ae836b8b5dc4b542783f070
khk9742 <hkkim@vitcom.co.kr>
2018-12-21 13:09:30 +0900

    금액 추가

---
+++ KcFinset/client/src/components/_sample/Money.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <section>
+      <div class="container pb90">
+        <ul class="consume-detail">
+          <div>
+            <li>
+              <p class="key">금액1</p>
+              <p>
+                <money :theme="'auto'" :readonly="true" v-model="money.first" />
+                <em>원</em>
+              </p>
+            </li>
+          </div>
+          <div>
+            <li>
+              <p class="key">금액2</p>
+              <p>
+                <money v-model="money.second" />
+                <em>원</em>
+              </p>
+            </li>
+          </div>
+          <div>
+            <li>
+              <p class="key">금액3</p>
+              <p>
+                <money v-model="money.third" />
+                <em>원</em>
+              </p>
+            </li>
+          </div>
+        </ul>
+      </div>
+    </section>
+  </div>
+</template>
+
+
+<script>
+import Common from "@/assets/js/common.js";
+import Constant from "@/assets/js/constant.js";
+import datepicker from "vuejs-datepicker";
+import { ko } from "vuejs-datepicker/dist/locale";
+
+export default {
+  name: "SampleDatepicker",
+  data() {
+    return {
+      Common: Common,
+      ko: ko,
+      money: {
+        first: "11",
+        second: null,
+        third: null
+      }
+    };
+  },
+  components: {},
+  computed: {},
+  beforeCreate() {
+    this.$store.state.header.type = "sub";
+    this.$store.state.title = "데이트피커 예시";
+  },
+  created() {},
+  beforeMount() {},
+  mounted() {
+    console.log(this.$router);
+    console.log(window.history);
+    Common.datepickerInit("div-date", this);
+  },
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {
+    openDatepicker: function(index) {
+      if (index == 1) {
+        this.$refs.datepicker1.showCalendar();
+      } else if (index == 2) {
+        this.$refs.datepicker2.showCalendar();
+      } else if (index == 3) {
+        this.$refs.datepicker3.showCalendar();
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style lang="scss">
+</style>
---
+++ KcFinset/client/src/components/plugins/money/Money.vue
@@ -0,0 +1,78 @@
+<template>
+  <input type="text" inputmode="numeric" pattern="[0-9]*" v-model="formatedValue" :placeholder="placeholder" :class="theme" :readonly="readonly" :disabled="disabled" />
+</template>
+
+<script>
+export default {
+  name: "Money",
+  props: {
+    // 표현되는 숫자 값
+    value: {
+      type: String
+    },
+    // placeholder 문구
+    placeholder: {
+      type: String,
+      required: false
+    },
+    // 클래스 명
+    theme: {
+      type: String,
+      required: false,
+      default: ""
+    },
+    // readonly 설정 여부
+    readonly: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    // disabled 설정 여부
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  watch: {},
+  data() {
+    return {
+      orgVal: ""
+    };
+  },
+  components: {},
+  computed: {
+    formatedValue: {
+      get: function() {
+        if ((this.value || "") != "") {
+          var numVal = parseInt(this.orgVal);
+          if (!isNaN(numVal)) {
+            return numVal.toLocaleString();
+          } else {
+            return "";
+          }
+        } else {
+          return "";
+        }
+      },
+      set: function(val) {
+        this.orgVal = val.replace(/,/g, "");
+        this.$emit("input", this.orgVal);
+      }
+    }
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {}
+};
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style lang="scss">
+</style>

ae79507b1f125b41c3c86913fc562264a841a0b3
minkyng <minkyoung9203@gmail.com>
2018-12-26 15:35:47 +0900

    validate 수정

--- KcFinset/client/src/components/assets/DirInput.vue
+++ KcFinset/client/src/components/assets/DirInput.vue
@@ -8,11 +8,11 @@
             <ul class="flex">
               <li class="key">분류</li>
               <li class="value">
-                <multiselect :id="'cd_assets_class'" v-model="cd_assets_class" ref="cd_assets_class" placeholder="분류선택" :title="'분류'" :options="assetsClassOptions" :onClose="selectAssetsClass" v-validate="'required'" data-vv-name='분류'>
+                <multiselect :id="'cd_assets_class'" v-model="cd_assets_class" ref="cd_assets_class" key="cd_assets_class" placeholder="분류선택" :title="'분류'" :options="assetsClassOptions" :onClose="selectAssetsClass" v-validate="'multi'" data-vv-name='분류'>
                 </multiselect>
               </li>
             </ul>
-            <p class="warn" v-if="errors.has('분류')">{{errors.first('분류')}}</p>
+            <p class="warn" v-if="errors.has('분류')" v-html="'분류 항목은 필수 정보입니다.'"></p>
           </div>
 
           <!--부동산-->
@@ -21,18 +21,18 @@
               <ul class="flex">
                 <li class="key">보유</li>
                 <li class="value">
-                  <multiselect :id="'cd_class_prop'" v-model="cd_class_prop" ref="cd_class_prop" :title="'보유'" :options="option_class_prop" v-validate="'required'" data-vv-name='보유'>
+                  <multiselect :id="'cd_class_prop'" v-model="cd_class_prop" ref="cd_class_prop" key="cd_class_prop" :title="'보유'" :options="option_class_prop" v-validate="'multi'" data-vv-name='보유'>
                   </multiselect>
                 </li>
               </ul>
-              <p class="warn" v-if="errors.has('보유')">{{errors.first('보유')}}</p>
+              <p class="warn" v-if="errors.has('보유')" v-html="'보유 항목은 필수 정보입니다.'"></p>
             </div>
             <div>
               <ul class="flex">
                 <li class="key">주소</li>
                 <li class="value">
                   <input type="text" v-model="view_addr" readonly="readonly">
-                  <input type="hidden" v-model="real_estate_addr" v-validate="'required'" data-vv-name='주소'>
+                  <input type="hidden" v-model="real_estate_addr" key="real_estate_addr" v-validate="'required'" data-vv-name='주소'>
                   <button class="search" @click="scAddress()"></button>
                 </li>
               </ul>
@@ -45,7 +45,7 @@
             <ul class="flex">
               <li class="key">모델명</li>
               <li class="value">
-                <input type="text" v-model="nm_model" v-validate="'required|max:15'" data-vv-name='모델명'>
+                <input type="text" v-model="nm_model" key="nm_model" v-validate="'required|max:15'" data-vv-name='모델명'>
               </li>
             </ul>
             <p class="warn" v-if="errors.has('모델명')">{{errors.first('모델명')}}</p>
@@ -57,17 +57,17 @@
               <ul class="flex">
                 <li class="key">종류</li>
                 <li class="value">
-                  <multiselect :id="'cd_class_nbmt'" v-model="cd_class_nbmt" ref="cd_class_nbmt" :title="'종류'" :options="option_class_nbmt" v-validate="'required'" data-vv-name='종류'>
+                  <multiselect :id="'cd_class_nbmt'" v-model="cd_class_nbmt" ref="cd_class_nbmt" key="cd_class_nbmt" :title="'종류'" :options="option_class_nbmt" v-validate="'multi'" data-vv-name='종류'>
                   </multiselect>
                 </li>
               </ul>
-              <p class="warn" v-if="errors.has('종류')">{{errors.first('종류')}}</p>
+              <p class="warn" v-if="errors.has('종류')" v-html="'종류 항목은 필수 정보입니다.'"></p>
             </div>
             <div>
               <ul class="flex">
                 <li class="key">보유량</li>
                 <li class="value">
-                  <input type="number" v-model="amount_jewelry" v-validate="'required|numeric|max:12'" data-vv-name='보유량'>g
+                  <input type="number" v-model="amount_jewelry" key="amount_jewelry" v-validate="'required|numeric|max:12'" data-vv-name='보유량'>g
                 </li>
               </ul>
               <p class="warn" v-if="errors.has('보유량')">{{errors.first('보유량')}}</p>
@@ -80,18 +80,17 @@
               <ul class="flex">
                 <li class="key">통화</li>
                 <li class="value">
-                  <multiselect :id="'cd_class_frcr'" v-model="cd_class_frcr" ref="cd_class_frcr" :title="'종류'" :options="option_class_frcr" v-validate="'required'" data-vv-name='통화'>
+                  <multiselect :id="'cd_class_frcr'" v-model="cd_class_frcr" ref="cd_class_frcr" key="cd_class_frcr" :title="'종류'" :options="option_class_frcr" v-validate="'multi'" data-vv-name='통화'>
                   </multiselect>
                 </li>
               </ul>
-              <p class="warn" v-if="errors.has('통화')">{{errors.first('통화')}}</p>
+              <p class="warn" v-if="errors.has('통화')" v-html="'통화 항목은 필수 정보입니다.'"></p>
             </div>
             <div>
               <ul class="flex">
                 <li class="key">보유금액</li>
                 <li class="value">
-                  <!-- <input type="number" v-model="amt_balance" v-validate="'required|numeric|max:12'" data-vv-name='보유금액'>원 -->
-                  <money v-model="amt_balance" v-validate="'required|numeric|max:12'" data-vv-name='보유금액' />원
+                  <money v-model="amt_balance" key="amt_balance" v-validate="'required|numeric|max:12'" data-vv-name='보유금액' />원
                 </li>
               </ul>
               <p class="warn" v-if="errors.has('보유금액')">{{errors.first('보유금액')}}</p>
@@ -103,7 +102,7 @@
             <ul class="flex">
               <li class="key">종류</li>
               <li class="value">
-                <input type="text" v-model="etc_assets" v-validate="'required|max:15'" data-vv-name='종류'>
+                <input type="text" v-model="etc_assets" key="etc_assets" v-validate="'required|max:15'" data-vv-name='종류'>
               </li>
             </ul>
             <p class="warn" v-if="errors.has('종류')">{{errors.first('종류')}}</p>
@@ -111,26 +110,32 @@
 
           <!--공통-->
           <template v-if="showKey!=''">
-            <div>
+            <!--가격/환산금액-->
+            <div v-if="showKey=='60'">
               <ul class="flex">
-                <!--가격/환산금액-->
-                <li class="key">{{title_amt_evaluation}}</li>
+                <li class="key">환산금액</li>
                 <li class="value">
-                  <!-- <input type="number" v-model="amt_evaluation" v-validate="'required|numeric|max:12'" :data-vv-name='title_amt_evaluation'>원 -->
-                  <money v-model="amt_evaluation" v-validate="'required|numeric|max:12'" :data-vv-name='title_amt_evaluation' />원
+                  <money v-model="amt_evaluation_frcr" key="amt_evaluation_frcr" v-validate="'required|numeric|max:12'" data-vv-name='환산금액' />원
                 </li>
               </ul>
-              <p class="warn" v-if="errors.has(title_amt_evaluation)">{{errors.first(title_amt_evaluation)}}</p>
+              <p class="warn" v-if="errors.has('환산금액')">{{errors.first('환산금액')}}</p>
             </div>
-            <div>
-              <ul class="block">
-                <li class="key">메모</li>
+            <div v-else>
+              <ul class="flex">
+                <li class="key">가격</li>
                 <li class="value">
-                  <input type="text" v-model="memo" v-validate="'max:15'" data-vv-name='메모'>
+                  <money v-model="amt_evaluation" key="amt_evaluation" v-validate="'required|numeric|max:12'" data-vv-name='가격' />원
                 </li>
               </ul>
-              <p class="warn" v-if="errors.has('메모')">{{errors.first('메모')}}</p>
+              <p class="warn" v-if="errors.has('가격')">{{errors.first('가격')}}</p>
             </div>
+            <ul class="block">
+              <li class="key">메모</li>
+              <li class="value">
+                <input type="text" v-model="memo" key="memo" v-validate="'max:15'" data-vv-name='메모'>
+                <p class="warn" v-if="errors.has('메모')">{{errors.first('메모')}}</p>
+              </li>
+            </ul>
           </template>
         </div>
 
@@ -186,9 +191,9 @@ export default {
       option_class_frcr: "", //외화 - 통화(option)
       cd_class_frcr: "", //외화 - 통화
       amt_balance: "", //외화 - 보유금액
+      amt_evaluation_frcr: "", //외화 - 환산금액
       etc_assets: "", //기타 - 종류
-      title_amt_evaluation: "",
-      amt_evaluation: "", //공통 - 가격, 외화 - 환산금액
+      amt_evaluation: "", //공통 - 가격
       memo: "" //공통 - 메모
     };
   },
@@ -236,12 +241,6 @@ export default {
       _this.errors.clear();
       _this.initForm();
 
-      if ("60" == param.value) {
-        _this.title_amt_evaluation = "환산금액";
-      } else {
-        _this.title_amt_evaluation = "가격";
-      }
-
       _this.showKey = param.value;
 
       _this.isShowBtn = true;
@@ -256,8 +255,8 @@ export default {
       _this.nm_model = "";
       _this.amount_jewelry = "";
       _this.amt_balance = "";
+      _this.amt_evaluation_frcr = "";
       _this.etc_assets = "";
-      _this.title_amt_evaluation = "";
       _this.amt_evaluation = "";
       _this.memo = "";
     },
@@ -305,7 +304,11 @@ export default {
                 formData.append("amount_jewelry", _this.amount_jewelry);
                 formData.append("amt_balance", _this.amt_balance);
                 formData.append("etc_assets", _this.etc_assets);
-                formData.append("amt_evaluation", _this.amt_evaluation);
+                if ("60" == cd_assets_class) {
+                  formData.append("amt_evaluation", _this.amt_evaluation_frcr);
+                } else {
+                  formData.append("amt_evaluation", _this.amt_evaluation);
+                }
                 formData.append("memo", _this.memo);
 
                 this.$http

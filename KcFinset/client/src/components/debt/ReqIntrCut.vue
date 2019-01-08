b5089b83e1fba3214173ac34c6f090dc003b5564
minkyng <minkyoung9203@gmail.com>
2019-01-08 17:28:22 +0900

    부채_금리인하요구권_검색시 toast실행후 최상단으로 이동

--- KcFinset/client/src/components/debt/ReqIntrCut.vue
+++ KcFinset/client/src/components/debt/ReqIntrCut.vue
@@ -414,6 +414,10 @@ export default {
           _this.debtList = list;
 
           _this.isInit = false;
+          _this.$toast.center("검색이 완료되었습니다.");
+          // setTimeout(function() {
+          $(document).scrollTop(0);
+          // }, 500);
 
           console.log(_this.isInit);
           console.log(_this.debtList);

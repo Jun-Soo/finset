c1e8ae84d38045f6dee4c31300b6eac39d9c149e
박준수 <ysmspjs@naver.com>
2019-01-10 17:30:13 +0900

    메인 css 수정

--- KcFinset/client/src/assets/js/constant.js
+++ KcFinset/client/src/assets/js/constant.js
@@ -5,8 +5,9 @@ export default {
   userAgent: '',
   params: {},
   options: {
-    title: 'FINSET',
+    title: '안내',
     cancelLabel: '취소',
-    okLabel: '확인'
+    okLabel: '확인',
+    textAreaClass: 'large'
   }
 }
